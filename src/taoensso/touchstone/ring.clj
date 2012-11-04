0cae281f723f71e579366e5c54cf1eaf25cffc35
Peter Taoussanis <ptaoussanis@gmail.com>
2012-11-04 20:00:39 +0700

    Add `wrap-random-test-subject-id*`
    Excludes bot-checking to improve middleware composability.

--- src/taoensso/touchstone/ring.clj
+++ src/taoensso/touchstone/ring.clj
@@ -4,25 +4,37 @@
   (:require [clojure.string      :as str]
             [taoensso.touchstone :as touchstone]))
 
-(defn wrap-random-test-subject-id
+(defn wrap-random-test-subject-id*
   "Wraps Ring handler to randomly generate, sessionize, and bind a test-subject
-  id for request. Request's User-Agent header will be checked, and bots excluded
-  from split-testing by having their id set to nil. Manually sessionize a nil
+  id for request."
+  [handler]
+  (fn [request]
+    (if (contains? (:session request) :mab-subject-id)
+
+      (let [sessionized-id (get-in request [:session :mab-subject-id])]
+        (touchstone/with-test-subject sessionized-id (handler request)))
+
+      (let [new-id   (str (rand-int 2147483647))
+            response (touchstone/with-test-subject new-id (handler request))]
+        (assoc-in response [:session :mab-subject-id] new-id)))))
+
+(defn bot-user-agent?
+  "Simple test for honest bots."
+  [ring-request-headers]
+  (boolean
+   (re-find
+    #"(agent|bing|bot|crawl|curl|facebook|google|index|slurp|spider|teoma|wget)"
+    (str/lower-case (get ring-request-headers "user-agent" "")))))
+
+(comment (bot-user-agent? {"user-agent" "GoogleBot"}))
+
+(defn wrap-random-test-subject-id
+  "Like `wrap-random-test-subject-id*` but checks request's User-Agent header to
+  exclude honest bots from split-testing. Manually sessionize a nil
   :mab-subject-id to exclude staff or bots detected via more sophisticated
   methods."
   [handler]
   (fn [request]
-    (if (contains? (:session request) :mab-subject-id)
-      (let [sessionized-id (get-in request [:session :mab-subject-id])]
-        (touchstone/with-test-subject sessionized-id (handler request)))
-
-      ;; Handle with then sessionize a new, randomly-generated id
-      (let [user-agent (-> (get-in request [:headers "user-agent"] "")
-                           str/lower-case)
-            known-bots-regex
-            #"(agent|bing|bot|crawl|curl|facebook|google|index|slurp|spider|teoma|wget)"
-
-            new-id (when-not (re-find known-bots-regex user-agent)
-                     (str (rand-int 2147483647))) ; nil for bots
-            response (touchstone/with-test-subject new-id (handler request))]
-        (assoc-in response [:session :mab-subject-id] new-id)))))
+    (if (bot-user-agent? (:headers request))
+      (handler request)
+      ((wrap-random-test-subject-id* handler) request))))
