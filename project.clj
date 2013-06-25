d572191a8a0efba88c20b26aee8c168479012f0a
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-25 15:25:04 +0700

    Clean up project.clj, add Clojure 1.6 test

--- project.clj
+++ project.clj
@@ -5,14 +5,29 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure            "1.4.0"]
                  [org.clojure/tools.macro        "0.1.2"]
-                 [com.taoensso/carmine           "1.7.0"]
-                 [org.clojure/math.combinatorics "0.0.3"]]
+                 [org.clojure/math.combinatorics "0.0.3"]
+                 [expectations                   "1.4.43"]
+                 [com.taoensso/carmine           "1.12.0"]]
   :profiles {:1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5   {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6   {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev   {:dependencies []}
              :test  {:dependencies []}
              :bench {:dependencies []}}
-  :aliases {"test-all" ["with-profile" "test,1.4:test,1.5" "test"]}
-  :plugins [[codox "0.6.4"]]
+  :aliases {"test-all"    ["with-profile" "test,1.4:test,1.5:test,1.6" "expectations"]
+            "test-auto"   ["with-profile" "test" "autoexpect"]
+            "start-dev"   ["with-profile" "dev,test" "repl" ":headless"]}
+  :plugins [[lein-expectations "0.0.7"]
+            [lein-autoexpect   "0.2.5"]
+            [codox             "0.6.4"]]
   :min-lein-version "2.0.0"
-  :warn-on-reflection true)
+  :warn-on-reflection true
+  :repositories
+  {"sonatype"
+   {:url "http://oss.sonatype.org/content/repositories/releases"
+    :snapshots false
+    :releases {:checksum :fail}}
+   "sonatype-snapshots"
+   {:url "http://oss.sonatype.org/content/repositories/snapshots"
+    :snapshots true
+    :releases {:checksum :fail :update :always}}})
---
+++ test/taoensso/touchstone/tests/main.clj
@@ -0,0 +1,5 @@
+(ns taoensso.touchstone.tests.main
+  (:require [expectations        :as test       :refer :all]
+            [taoensso.touchstone :as touchstone :refer ()]))
+
+(expect true) ; TODO Add tests (PRs welcome!)
