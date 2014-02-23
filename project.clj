db2687028b5e5e929dc826d0823d07c74d69ee8e
Peter Taoussanis <ptaoussanis@gmail.com>
2014-02-23 20:24:20 +0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,29 +1,49 @@
 (defproject com.taoensso/touchstone "2.0.1"
+  :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure A/B testing library"
   :url "https://github.com/ptaoussanis/touchstone"
   :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure            "1.4.0"]
-                 [org.clojure/tools.macro        "0.1.5"]
-                 [org.clojure/math.combinatorics "0.0.7"]
-                 [com.taoensso/carmine           "2.4.0"]]
-  :profiles {:1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5   {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6   {:dependencies [[org.clojure/clojure "1.6.0-alpha2"]]}
-             :dev   {:dependencies []}
-             :test  {:dependencies [[expectations   "1.4.56"]
-                                    [ring/ring-core "1.2.1"]]}
-             :bench {:dependencies []}}
-  :aliases {"test-all"    ["with-profile" "+test,+1.4:+test,+1.5:+test,+1.6" "expectations"]
-            "test-auto"   ["with-profile" "+test" "autoexpect"]
-            "start-dev"   ["with-profile" "+dev,+test" "repl" ":headless"]
-            "codox"       ["with-profile" "+test" "doc"]}
-  :plugins [[lein-expectations "0.0.8"]
-            [lein-autoexpect   "1.0"]
-            [lein-ancient      "0.5.4"]
-            [codox             "0.6.6"]]
-  :min-lein-version "2.0.0"
-  :global-vars {*warn-on-reflection* true}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "Same as Clojure"}
+  :min-lein-version "2.3.3"
+  :global-vars {*warn-on-reflection* true
+                *assert* true}
+  :dependencies
+  [[org.clojure/clojure            "1.4.0"]
+   [org.clojure/tools.macro        "0.1.5"]
+   [org.clojure/math.combinatorics "0.0.7"]
+   [com.taoensso/encore            "0.8.0"]
+   [com.taoensso/carmine           "2.4.6"]]
+
+  :test-paths ["test" "src"]
+  :profiles
+  {:build {:hooks ^:replace []} ; Workaround to avoid :dev hooks during deploy
+   :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
+   :1.6  {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}
+   :test {:dependencies [[expectations            "1.4.56"]
+                         [reiddraper/simple-check "0.5.6"]
+                         [ring/ring-core          "1.2.1"]]
+          :plugins [[lein-expectations "0.0.8"]
+                    [lein-autoexpect   "1.2.2"]]}
+   :dev
+   [:1.6 :test
+    {:jvm-opts ^:replace ["-server"]
+     :hooks []
+     :dependencies []
+     :plugins []}]}
+
+  :plugins [[lein-ancient "0.5.4"]
+            [codox        "0.6.7"]]
+
+  ;; :codox {:sources ["target/classes"]} ; For use with cljx
+  :aliases
+  {"test-all"   ["with-profile" "+test:+1.5,+test:+1.6,+test" "expectations"]
+   "test-auto"  ["with-profile" "+test" "autoexpect"]
+   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]
+   "codox"      ["with-profile" "+test" "doc"]
+   "deploy-lib" ["with-profile" "+dev,+build" "do" "deploy" "clojars," "install"]}
+
   :repositories
   {"sonatype"
    {:url "http://oss.sonatype.org/content/repositories/releases"
