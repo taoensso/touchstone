9a3e5a4acbfa6d29679933aabfef15173b115e74
Peter Taoussanis <ptaoussanis@gmail.com>
2014-02-23 22:10:43 +0700

    Tests hk

--- test/taoensso/touchstone/tests/main.clj
+++ test/taoensso/touchstone/tests/main.clj
@@ -2,6 +2,8 @@
   (:require [expectations        :as test       :refer :all]
             [taoensso.touchstone :as touchstone :refer ()]))
 
+(comment (test/run-tests '[taoensso.touchstone.tests.main]))
+
 (defn- before-run {:expectations-options :before-run} [])
 (defn- after-run  {:expectations-options :after-run}  [])
 
